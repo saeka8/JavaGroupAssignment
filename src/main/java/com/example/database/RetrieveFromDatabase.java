d2d2ce943215a3ceecc0d0ed28bfa0614cb5fcc1
Sofiia Avetisian <savetisian.ieu2023@student.ie.edu>
2025-12-03 21:39:51 +0100

    delete the unused method

--- src/main/java/com/example/database/RetrieveFromDatabase.java
+++ src/main/java/com/example/database/RetrieveFromDatabase.java
@@ -16,49 +16,6 @@ import static com.example.model.User.Role.*;
 public class RetrieveFromDatabase {
 
     // ===== Reading data =====
-    // Retrieve a user based on email and password
-    private static User retrieveUser(Connection conn, String email, String password) throws SQLException {
-        String sql = "SELECT id, name, lastname, role FROM people WHERE email='" + email + "' AND password='" + password + "'";
-        User user = null;
-        try (Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery(sql)) { // executeQuery returns data
-            // Loop through the result set. rs.next() returns false when there are no more rows.
-            while (rs.next()) {
-                int id = rs.getInt("id");
-                String name = rs.getString("name");
-                String lastname = rs.getString("lastname");
-                String role = rs.getString("role");
-                User.Role user_role;
-                if (role.equalsIgnoreCase("admin") ){
-                    user_role = ADMIN;
-                } else if (role.equalsIgnoreCase("teacher")) {
-                    user_role = TEACHER;
-                }else {
-                    user_role = STUDENT;
-                }
-                user = User.createUser(id, email, password, name, lastname, user_role);
-            }
-        }
-        return user;
-    }
-
-    // Retrieve Groups based on Teacher ID
-    private static Map<Integer,String> retrieveGroup(Connection conn, User teacher) throws SQLException {
-        int teacher_id = teacher.getId();
-        Map<Integer,String> groups = new HashMap<>();
-        String sql = "SELECT id, name FROM groups WHERE teacher_id=" + teacher_id;
-        try (Statement stmt = conn.createStatement();
-             ResultSet rs = stmt.executeQuery(sql)) { // executeQuery returns data
-            // Loop through the result set. rs.next() returns false when there are no more rows.
-            while (rs.next()) {
-                int id = rs.getInt("id");
-                String name = rs.getString("name");
-                groups.put(id,name);
-            }
-        }
-        return groups;
-    }
-
     // Retrieve teacher ID by email
     public static int getTeacherId(Connection conn, String teacherEmail) throws SQLException {
         String sql = "SELECT id FROM people WHERE email='" + teacherEmail + "' AND role='teacher'";
