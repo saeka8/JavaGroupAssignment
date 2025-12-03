bab214cbd1badbf008cd924d47a4c02a14350835
Simonida Jekic <sjekic.ieu2023@student.ie.edu>
2025-12-03 23:00:40 +0100

    create jar file

---
+++ src/main/java/com/example/Launcher.java
@@ -0,0 +1,11 @@
+package com.example;
+
+public class Launcher {
+    public static void main(String[] args) {
+        try {
+            Main.main(args);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
